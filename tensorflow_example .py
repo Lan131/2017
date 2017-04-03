a7973f9fa8091bc345fb9f9019a5ede144aa599c
mlanier <mlanier131@gmail.com>
2017-04-03 13:31:27 -0500

    Add files via upload

---
+++ tensorflow_example .py
@@ -0,0 +1,61 @@
+
+# coding: utf-8
+
+# In[12]:
+
+
+
+import numpy as np
+
+num_points = 1000
+vectors_set = []
+for i in range(num_points):
+         x1= np.random.normal(0.0, 0.55)
+         y1= x1 * 0.1 + 0.3 + np.random.normal(0.0, 0.03)
+         vectors_set.append([x1, y1])
+
+x_data = [v[0] for v in vectors_set]
+y_data = [v[1] for v in vectors_set]
+
+import matplotlib.pyplot as plt
+
+#Graphic display
+plt.plot(x_data, y_data, 'ro')
+plt.legend()
+plt.show()
+
+import tensorflow as tf
+
+W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+b = tf.Variable(tf.zeros([1]))
+y = W * x_data + b
+
+loss = tf.reduce_mean(tf.square(y - y_data))
+optimizer = tf.train.AdamOptimizer(learning_rate=0.1, beta1=0.9, beta2=0.999, epsilon=1e-08)
+train = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init)
+
+for step in range(100):
+     sess.run(train)
+     print(step, sess.run(W), sess.run(b))
+     print(step, sess.run(loss))
+
+     #Graphic display
+     plt.plot(x_data, y_data, 'ro')
+     plt.plot(x_data, sess.run(W) * x_data + sess.run(b))
+     plt.xlabel('x')
+     plt.xlim(-2,2)
+     plt.ylim(0.1,0.6)
+     plt.ylabel('y')
+     plt.legend()
+     plt.show()
+
+
+# In[ ]:
+
+
+
