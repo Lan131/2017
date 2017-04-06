926cf387cade221b7586cfc6748b835ee2e75d72
mlanier <mlanier131@gmail.com>
2017-04-06 12:42:56 -0500

    Update common.sql

--- common.sql
+++ common.sql
@@ -32,3 +32,45 @@ SELECT ColumnName
 FROM DBC.COLUMNS
 WHERE DatabaseName='XBITBLSv' 
 AND TABLENAME='FctSysTechWOJobMthMbl ';
+
+
+SELECT	TOP 5 A.ThruFsclMth AS D, A.HrNum AS ID,  A.ProdAct AS Actual_Productivity,
+		A.OTAAct AS Actual_OTA,A.ResPosRank AS Rank_resi_pos,
+A.ResPosTotalTech AS Total_tech_resi_pos, A.ResTCRank AS RANK_Resi_Trouble,
+		A.ResTCTotalTech AS Total_techs_Resi_Trouble,
+A.CBPosRank AS RANK_SB_positive,A.CBPosTotalTech AS Total_Techs_SB_positive,
+A.CBTCRank AS RANK_SB_trouble,A.CBTCTotalTech AS Total_Techs_SB_negative,
+		A.ProdRank AS Rank_Productivity ,
+
+A.ProdTotalTech AS Total_Productivity,A.OTARank,A.OTATotalTech,
+		A.ResPosAct,A.ResTCAct,A.CBPosAct,A.CBTCAct,
+A.ResPosPnts,A.ResTcPnts,A.CBPosPnts,A.CBTCPnts,A.ProdPnts,A.OTAPnts,
+		A.ResPosCnt,
+		A.ResTCCnt  ,  A.CBPosCnt , A.CBTCCnt, A.QuotaPnts, A.MtrcTmPeriod,
+		A.TotalPnts AS Total,B.*
+		
+		
+
+FROM
+
+(
+SELECT	BusDt, ThruFsclMth, ThruFsclMthSk, HrNum, DimFsclMthSk,
+		TechNm, TechTitle, TechSrcKMARef, KMADesc, DivisionDesc, SupHrNum,
+		SupNm, SupTitle, LOADays, TmInPosition, MtrcTmPeriod, ScrcdGrp,
+		ResPosCnt, ResTCCnt, CBPosCnt, CBTCCnt, TotalQualWOCnt, RwrkResPosCnt,
+		RwrkResTCCnt, RwrkCBPosCnt, RwrkCBTCCnt, ResPosAct, ResTCAct,
+		CBPosAct, CBTCAct, SupQual, SupThrs, ResPosTotalTech, ResTCTotalTech,
+		CBPosTotalTech, CBTCTotalTech, ResPosRank, ResTCRank, CBPosRank,
+		CBTCRank, ResPosPnts, ResTcPnts, CBPosPnts, CBTCPnts, WghtRwrkPnts,
+		ResPosPerc, ResTCPerc, CBPosPerc, CBTCPerc, WghtResPosPnts, WghtResTcPnts,
+		WghtCBPosPnts, WghtCBTCPnts, QuotaPnts, ProdMins, ProdAct, ProdTotalTech,
+		ProdRank, ProdPnts, OTAYes, OTANo, OTAAct, OTATotalTech, OTARank,
+		OTAPnts, RwrkScrcrdPnts, ProdScrcrdPnts, OTAScrcrdPnts, TotalTechs,
+		TotalPnts, ScrcrdRank, TechScrcrdCurrTier, ExecStatId, SnapDttm,
+		ExecBatchId, InsDttm
+FROM XBIAQINTRPTV.RptBBTScrcrdSupMthLgcy)  AS A INNER JOIN 
+
+XBIAQINTRPTV.RptBBTScrcrdSupMthLgcy AS B 
+	ON A.HrNum=B.HrNum
+WHERE  A.MtrcTmPeriod=B.MtrcTmPeriod 
+ORDER BY A.HrNum 
