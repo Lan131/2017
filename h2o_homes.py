5db47ea3989e3553c47c012285824d717aedb638
mlanier <mlanier131@gmail.com>
2017-06-15 10:02:14 -0500

    Create h2o_homes.py

--- h2o_homes.py
+++ h2o_homes.py
@@ -1,25 +1,24 @@
 
-# coding: utf-8
 
-# In[1]:
 
+##revert to h2o	3.10.0.9
 import h2o
 from h2o.estimators.deeplearning import H2ODeepLearningEstimator
 h2o.init()
 
 
-# In[2]:
+
 
 data = h2o.import_file("merged_data.csv")
 
 
 
-# In[8]:
+
 
 data=data[data['transactiondate']]
 
 
-# In[9]:
+
 
 data=data.drop(0)
 data=data.drop(0)
@@ -30,7 +29,7 @@ data.describe()
 
 
 
-# In[24]:
+
 
 data[u'logerror'].hist()
 for x in data.col_names[0:57]:
@@ -38,7 +37,7 @@ for x in data.col_names[0:57]:
         data[x].hist()
 
 
-# In[25]:
+
 
 splits = data.split_frame(ratios=[0.70,.15], seed=1234)
 train  = h2o.assign(splits[0], "train.hex") # 70%
@@ -48,19 +47,18 @@ X = data.col_names[0:56]
 #X.append(data.col_names[58])
 
 
-# In[26]:
+
 
 
 dl = H2ODeepLearningEstimator(distribution="laplace",variable_importances=True)
 dl.train(x=X, y="logerror", training_frame=train,validation_frame=valid,nfolds=5,epochs=3,l1=.0001,l2=.0001,loss='Absolute')
 
 
-# In[25]:
+
 
 dl
 
 
-# In[ ]:
 
 
 
